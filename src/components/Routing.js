1b8a0c1f3fb16175793828fa962616f1a10e4f8d
Tim Jones <tim.jones@credera.co.uk>
2023-03-10 15:45:43 +0000

    feat: add route

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -4,7 +4,7 @@ import Results from "./Results";
 
 export const Routing = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/" element={<SearchPage />} />
         <Route path="/results" element={<Results />} />
