84f19f174df56603b9538e18b1ab44ee94876f7b
Tim Jones <tim.jones@credera.co.uk>
2023-03-10 11:28:53 +0000

    feat: template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Blog from "./components/Blog";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Blog />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
