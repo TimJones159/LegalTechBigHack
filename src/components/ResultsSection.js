47790ef219d08d6f35392fd6614f3e4784bf53a9
Shreyas Honnalli <sc20sh@leeds.ac.uk>
2023-03-10 15:14:40 +0000

    Changed results

--- src/components/ResultsSection.js
+++ src/components/ResultsSection.js
@@ -4,31 +4,31 @@ import SearchSection from "./SearchSection";
 import Result from "./Result";
 
 export const ResultsSection = ({ results }) => {
-  return (
-    <>
-      <div className="resultsSection">
-        <span>
-          <Link href="/">Home</Link> / Results
-        </span>
-        <SearchSection small />
-      </div>
-      <div className="resultsSort">
-        <div>
-          <span className="heading">Sort by:</span>
-          <span className="active">Relevance</span>
-          <span>Year</span>
-        </div>
-        <div>
-          <span className="totalResults">56 results found</span>
-        </div>
-      </div>
-      <div>
-        {results.map((result) => (
-          <Result key={result.title} {...result} />
-        ))}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="resultsSection">
+				<span>
+					<Link href="/">Home</Link> / Results
+				</span>
+				<SearchSection small />
+			</div>
+			<div className="resultsSort">
+				<div>
+					<span className="heading">Sort by:</span>
+					<span className="active">Relevance</span>
+					<span>Year</span>
+				</div>
+				<div>
+					<span className="totalResults">6 results found</span>
+				</div>
+			</div>
+			<div>
+				{results.map((result) => (
+					<Result key={result.title} {...result} />
+				))}
+			</div>
+		</>
+	);
 };
 
 export default ResultsSection;
