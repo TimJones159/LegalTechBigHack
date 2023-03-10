7fd1e80235508b524e59e0caac626f967ed32f5b
Tim Jones <tim.jones@credera.co.uk>
2023-03-10 13:43:01 +0000

    feat: logo click

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,33 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 
 function Header(props) {
   const { title } = props;
   const intro = "Hello, LawyerName!";
   return (
     <React.Fragment>
-      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider', boxShadow: '0 2px 8px 0 rgba(0,0,0,0.2)' }}>
-        <div style={{ display: 'flex', alignItems: 'center', flex: 1 }}>
+      <Toolbar
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+          boxShadow: "0 2px 8px 0 rgba(0,0,0,0.2)",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center", flex: 1 }}>
           <div className="header__logo">
-            <img
-              src="/logo.png"
-              alt="Logo"
-              style={{ maxHeight: '50px', marginRight: '8px' }}
-            />
+            <Link href="/">
+              <img
+                src="/logo.png"
+                alt="Logo"
+                style={{ maxHeight: "50px", marginRight: "8px" }}
+              />
+            </Link>
           </div>
           <Typography
             component="h2"
@@ -37,16 +45,10 @@ function Header(props) {
           color="inherit"
           align="center"
           noWrap
-          sx={{ marginRight: '35px' }}
-
-        >
-        </Typography>
-        <Button size="small">
-          My library
-        </Button>
-        <Button size="small">
-          Settings
-        </Button>
+          sx={{ marginRight: "35px" }}
+        ></Typography>
+        <Button size="small">My library</Button>
+        <Button size="small">Settings</Button>
         <Button variant="outlined" size="small">
           Log out
         </Button>
@@ -54,9 +56,8 @@ function Header(props) {
       <Toolbar
         component="nav"
         variant="dense"
-        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
-      >
-      </Toolbar>
+        sx={{ justifyContent: "space-between", overflowX: "auto" }}
+      ></Toolbar>
     </React.Fragment>
   );
 }
